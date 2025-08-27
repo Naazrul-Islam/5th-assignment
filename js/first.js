a479de2af3e5080f2e151e5fb050aca6a50189fb
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-08-27 09:38:52 -0700

    call and copy button added

--- js/first.js
+++ js/first.js
@@ -7,3 +7,64 @@ for(heart of hearts){
         countSpan.innerText = count;
     });
 }
+// **************call btn****************
+
+let stars = 100;
+let callbtns = document.getElementsByClassName("call-btn");
+
+for (let callbtn of callbtns) {
+    callbtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        if (stars < 20) {
+            alert("no stars");
+            return;
+        }
+
+
+
+        let serviceNumber = this.parentNode.parentNode.querySelector(".sarvice-title").innerText;
+        let phoneNumber = this.parentNode.parentNode.querySelector(".phn-number").innerText
+        alert(`Calling ${serviceNumber} at ${phoneNumber}`);
+        stars -= 20;
+        document.getElementById("star-count").innerText = stars;
+
+
+
+
+
+        let now = new Date();
+        let time = now.toLocaleTimeString();
+        let historyContainer = document.getElementById("history");
+        let newP = document.createElement("div");
+        newP.className = "call-history-entry flex justify-between items-center bg-[#FAFAFA] rounded-2xl p-[15px] mt-2 gap-3 max-h-83px";
+        newP.innerHTML = `<div>${serviceNumber} <br> ${phoneNumber}</div> <div>${time}</div>`;
+        historyContainer.appendChild(newP);
+    });
+}
+
+// ********************* clear button *********************
+
+
+let clearBtn = document.querySelector("#history button");
+clearBtn.addEventListener("click", function() {
+    let historyContainer = document.getElementById("history");
+    let entries = historyContainer.querySelectorAll(".call-history-entry");
+    entries.forEach(entry => entry.remove());
+});
+// ********************* copy button *********************
+let copyCounts = 0;
+let copyBtns = document.getElementsByClassName("copy-btn");
+
+for (let copyBtn of copyBtns) {
+    copyBtn.addEventListener("click", function() {
+        let phoneNumber = this.parentNode.parentNode.querySelector(".phn-number").innerText;
+        navigator.clipboard.writeText(phoneNumber).then(() => {;
+            alert(`Copied ${phoneNumber} to clipboard`);
+        });
+
+        let countContainer = document.getElementById("copy-count");
+        let count = parseInt(countContainer.innerText);
+        count++;
+        countContainer.innerText = count;
+    });
+}
